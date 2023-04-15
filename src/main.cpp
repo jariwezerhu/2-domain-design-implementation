1913f92e01d1c4749f1074607fe0be30e1c062cd
jariwezerhu <jari.wezer@student.hu.nl>
2023-04-15 22:06:33 +0200

    Some testing, all works as intended

--- src/main.cpp
+++ src/main.cpp
@@ -8,33 +8,58 @@
 using namespace std;
 
 int main() {
-    // Test if sort pallets works
-    Shelf shelf;
-    Pallet* pallet1 = new Pallet("item1", 10, 2);
-    Pallet* pallet2 = new Pallet("item2", 10, 7);
-    Pallet* pallet3 = new Pallet("item3", 10, 3);
-    Pallet* pallet4 = new Pallet("item4", 10, 4);
+    // Create a few random items
+    vector<string> items;
+    for (int i = 0; i < 10; i++) {
+        items.push_back("Item" + to_string(i));
+    }
 
-    shelf.insertPallet(0, pallet1);
-    shelf.insertPallet(1, pallet2);
-    shelf.insertPallet(2, pallet3);
-    shelf.insertPallet(3, pallet4);
-
-    array<bool, 4> slotStatus = shelf.getSlotStatus();
-    for (int i = 0; i < 4; i++) {
-        if (slotStatus[i]) {
-            cout << "Pallet " << i << " item count: " << shelf.pallets[i]->getItemCount() << endl;
+    // Create shelves
+    vector<Shelf> shelves;
+    for (int i = 0; i < 10; i++) {
+        shelves.push_back(Shelf());
+    }
+    // Fill shelves with pallets
+    for (int i = 0; i < shelves.size(); i++) {
+        for (int j = 0; j < 4; j++) {
+            // Randomly choose an item from the list
+            string item = items[rand() % items.size()];
+            // Randomly choose a capacity between 0 and 10
+            int capacity = rand() % 11;
+            // Randomly choose a count between 0 and the capacity
+            int count = rand() % (capacity + 1);
+            // Create a pallet with the item, capacity, and count
+            Pallet* pallet = new Pallet(item, capacity, count);
+            // Insert the pallet into the shelf
+            shelves[i].insertPallet(j, pallet);
         }
     }
 
-    shelf.sortPallets();
-
-    // Test if pallets are sorted
-    slotStatus = shelf.getSlotStatus();
-    for (int i = 0; i < 4; i++) {
-        if (slotStatus[i]) {
-            cout << "Pallet " << i << " item count: " << shelf.pallets[i]->getItemCount() << endl;
-        }
+    // Create some employees
+    vector<Employee> employees;
+    for (int i = 0; i < 10; i++) {
+        string name = "Employee" + to_string(i);
+        Employee employee = Employee(i, name, rand() % 2);
+        employees.push_back(employee);
     }
 
+    // Print employee information
+    cout << "Employees:" << endl;
+    for (int i = 0; i < employees.size(); i++) {
+        cout << "ID: " << employees[i].getID() << endl;
+        cout << "Name: " << employees[i].getName() << endl;
+        cout << "Forklift Certificate: " << employees[i].getForkliftCertificate() << endl;
+        cout << endl;
+    }
+
+    // Create pickItem task
+    pickItems pickItemsTask = pickItems("Item2", 10);
+
+    // Run pickItems task
+    bool success = pickItemsTask.runTask(employees[0], shelves);
+    if (success) {
+        cout << "Task completed successfully!" << endl;
+    } else {
+        cout << "Task failed!" << endl;
+    }
 }
